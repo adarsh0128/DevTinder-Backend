cb6630ecd4b86c8e231a83f1072e6b5c96e4260c
Adarsh kumar <adarshkumar0128@gmail.com>
2025-01-04 16:32:49 +0530

    explore routing

--- src/app.js
+++ src/app.js
@@ -2,10 +2,22 @@ const express = require("express");
 
 const app = express();
 
-app.use("/", (req, res) => {
-  res.send("Hi ADARSH");
+// this will only match get
+app.get("/user/:id/:pass", (req, res) => {
+  res.send("evrything is wright");
 });
 
+// this will only match get
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Adarsh", lastName: "Kumar" });
+});
+
+// this will only match post
+app.post("/user", (req, res) => {
+  res.send("Data saved successfully");
+});
+
+// this will match all the http method api call to test the user
 app.use("/home", (req, res) => {
   res.send("Hi welcome to home");
 });
