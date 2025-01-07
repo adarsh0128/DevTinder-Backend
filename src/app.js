2928746d92fe9a3faa4589b30c7bcb9d327bd150
Adarsh kumar <adarshkumar0128@gmail.com>
2025-01-08 00:05:15 +0530

    error handling

--- src/app.js
+++ src/app.js
@@ -1,34 +1,20 @@
 const express = require("express");
 
 const app = express();
-const { adminAuth, userAuth } = require("./middleware/auth");
-
-app.use("/admin/seedata", (req, res) => {
-  res.send("see data");
-});
-
-app.use("/user/seedata", (req, res) => {
-  res.send("see data");
-});
-
-app.use("/admin", adminAuth);
-
-app.use("/user", userAuth);
 
 app.use("/admin/getdata", (req, res) => {
-  res.send("get data");
+  // try {
+  //   throw new Error("ilqggev");
+  //   res.send("hi");
+  // } catch (error) {
+  //   res.status(500).send("error 1");
+  // }
+  throw new Error("ilqggev");
+  res.send("hi");
 });
 
-app.use("/user/getdata", (req, res) => {
-  res.send("get data");
-});
-
-app.use("/admin/deletedata", (req, res) => {
-  res.send("delete data");
-});
-
-app.use("/user/deletedata", (req, res) => {
-  res.send("delete data");
+app.use("/", (error, req, res, next) => {
+  res.status(500).send("error 2");
 });
 
 app.listen(3000, () => {
