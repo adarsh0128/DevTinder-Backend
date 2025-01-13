9a671cd227915092880c06ac6221c656bafa8c51
Adarsh kumar <adarshkumar0128@gmail.com>
2025-01-13 17:00:24 +0530

    database basics

--- src/app.js
+++ src/app.js
@@ -1,22 +1,32 @@
 const express = require("express");
-
+const connectDB = require("./config/database");
 const app = express();
+const User = require("./models/user");
 
-app.use("/admin/getdata", (req, res) => {
-  // try {
-  //   throw new Error("ilqggev");
-  //   res.send("hi");
-  // } catch (error) {
-  //   res.status(500).send("error 1");
-  // }
-  throw new Error("ilqggev");
-  res.send("hi");
+app.post("/signup", async (req, res) => {
+  const user = new User({
+    firstName: "Adarsh",
+    lastname: "Kumar",
+    emailId: "a@gmail.com",
+    password: "a123",
+    gender: "male",
+  });
+
+  try {
+    await user.save();
+    res.send("user added successfully");
+  } catch (error) {
+    res.status(400).send("error in saving data");
+  }
 });
 
-app.use("/", (error, req, res, next) => {
-  res.status(500).send("error 2");
-});
-
-app.listen(3000, () => {
-  console.log("success");
-});
+connectDB()
+  .then(() => {
+    console.log("Database coneect successfully");
+    app.listen(3000, () => {
+      console.log("success");
+    });
+  })
+  .catch((err) => {
+    console.log("database not connected");
+  });
