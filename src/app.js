32eae0eb820e06c7c6d7b092eb21358da3cb21bb
Adarsh kumar <adarshkumar0128@gmail.com>
2025-01-24 18:06:25 +0530

    data sanitazition

--- src/app.js
+++ src/app.js
@@ -53,25 +53,32 @@ app.delete("/user", async (req, res) => {
   }
 });
 
-app.patch("/user", async (req, res) => {
-  const userId = req.body.userId;
+app.patch("/user/:userId", async (req, res) => {
+  const userId = req.params?.userId;
   const data = req.body;
   try {
+    const ALLOWED_UPDATES = ["photoUrl", "about", "gender", "age", "skills"];
+    const isUpdateAllowed = Object.keys(data).every((k) =>
+      ALLOWED_UPDATES.includes(k)
+    );
+    if (!isUpdateAllowed) {
+      throw new Error("Update no allowed");
+    }
     const user = await User.findByIdAndUpdate({ _id: userId }, data, {
       returnDocument: "after",
       runValidators: true,
     });
-
-    res.send("user updated succesfully");
-  } catch (error) {
-    res.status(400).send("something went wrong");
+    console.log(user);
+    res.send("User updated successfully");
+  } catch (err) {
+    res.status(400).send("UPDATE FAILED:" + err.message);
   }
 });
 
 connectDB()
   .then(() => {
     console.log("Database coneect successfully");
-    app.listen(3003, () => {
+    app.listen(3005, () => {
       console.log("success");
     });
   })
