a9006f32d98c485b49eb0ff323a72751e28a601a
Adarsh kumar <adarshkumar0128@gmail.com>
2025-02-08 16:08:29 +0530

    authontication

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/user");
+const { validateSignUpData } = require("./utils/validation");
+const bcrypt = require("bcrypt");
 
 app.use(express.json());
 
@@ -35,10 +37,25 @@ app.post("/signup", async (req, res) => {
   const user = new User(req.body);
 
   try {
+    // validate data
+    validateSignUpData(req);
+
+    const { firstName, lastName, emailId, password } = req.body;
+    // Encrypt the password
+    const passwordHash = await bcrypt.hash(password, 10);
+    console.log(passwordHash);
+    //   Creating a new instance of the User model
+    const user = new User({
+      firstName,
+      lastName,
+      emailId,
+      password: passwordHash,
+    });
+
     await user.save();
     res.send("user added successfully");
-  } catch (error) {
-    res.status(400).send("error in saving data");
+  } catch (err) {
+    res.status(400).send("ERROOR :" + err.message);
   }
 });
 
@@ -78,7 +95,7 @@ app.patch("/user/:userId", async (req, res) => {
 connectDB()
   .then(() => {
     console.log("Database coneect successfully");
-    app.listen(3005, () => {
+    app.listen(3006, () => {
       console.log("success");
     });
   })
